e22fdf767ea3374e774513d7aded980627b1236a
MacKar93 <maciej.karwacki93@gmail.com>
2025-03-30 21:23:14 +0200

    Create basic dashboard layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
+import { DashboardHomeComponent } from './dashboard/dashboard-home/dashboard-home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+      { path: 'dashboard', component: DashboardHomeComponent },
+    ],
+  },
+];
